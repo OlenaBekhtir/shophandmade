a343be9af5b57014c21a33330a265c5e3cb72eeb
OlenaBekhtir <bekhtir@ukr.net>
2024-04-01 15:49:45 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ PHPMailer/language/phpmailer.lang-uk.php
@@ -0,0 +1,20 @@
+<?php
+
+$PHPMAILER_LANG['authenticate']         = 'Error';
+$PHPMAILER_LANG['connect_host']         = 'Error';
+$PHPMAILER_LANG['data_not_accepted']    = 'Error';
+$PHPMAILER_LANG['encoding']             = 'Error';
+$PHPMAILER_LANG['execute']              = 'Error';
+$PHPMAILER_LANG['file_access']          = 'Error';
+$PHPMAILER_LANG['file_open']            = 'Error';
+$PHPMAILER_LANG['from_failed']          = 'Error';
+$PHPMAILER_LANG['instantiate']          = 'Error';
+$PHPMAILER_LANG['provide_address']      = 'Error';
+$PHPMAILER_LANG['mailer_not_supported'] = 'Error';
+$PHPMAILER_LANG['recipients_failed']    = 'Error';
+$PHPMAILER_LANG['empty_message']        = 'Error';
+$PHPMAILER_LANG['invalid_address']      = 'Error';
+$PHPMAILER_LANG['signing']              = 'Error';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'Error';
+$PHPMAILER_LANG['smtp_error']           = 'Error';
+$PHPMAILER_LANG['variable_set']         = 'Error';
---
+++ js/main.js
@@ -0,0 +1,5 @@
+(()=>{var __webpack_modules__={"./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/()=>{eval("document.addEventListener('DOMContentLoaded', function () {});\n\n//# sourceURL=webpack://internet-shop/./src/js/main.js?")}},__webpack_exports__={};__webpack_modules__["./src/js/main.js"]()})();
+//# sourceMappingURL=main.js.map
